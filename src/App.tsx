0d855ccfe079eb572aaefbd11b39269143821b3d
yomik15 <yomiki15@outlook.com>
2026-05-15 18:42:20 -0500

    patter(case 2): encapsulate props layout: context props parent & layout childs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react"
 import { MessyApp } from "./messy"
 import { CompoundPatternApp } from "./compound-pattern/CompoundPatternApp"
+import { Consumer } from "./compound-pattern2/Consumer"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       {/* <MessyApp /> */}
-      <CompoundPatternApp />
+      {/* <CompoundPatternApp /> */}
+      <Consumer />
     </>
   )
 }
---
+++ src/compound-pattern2/Consumer.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import "../App.css"
+import Modal from "./Modal.tsx"
+export function Consumer() {
+  // const [isOpen, setIsOpen] = useState(false)
+
+  // const closeModal = () => {
+  //   setIsOpen(false)
+  // }
+
+  return (
+    <div className="flex flex-col items-center">
+      {/* <Modal isOpen={isOpen} closeModal={closeModal}> */}
+      <Modal>
+        {/* <button onClick={() => setIsOpen(true)}>Open Modal</button> */}
+        {/* <Modal.Trigger>Open Modal</Modal.Trigger> */}
+
+        <Modal.Close />
+        <Modal.Header>
+          {/* <h2>Delete Account</h2> */}
+          Delete Account
+        </Modal.Header>
+        <Modal.Body>
+          {/* <p>Are you sure to delete account?</p> */}
+          Are you sure to delete account?
+        </Modal.Body>
+        {/* <Modal.FooterBtns>        <Modal.FooterBtns> */}
+        {/**/}
+        {/*   <button onClick={closeModal}>Close </button> */}
+        {/*   <button onClick={() => alert("Action performed!")}>Do Action</button> */}
+        {/* </Modal.FooterBtns> */}
+        <Modal.FooterBtns />
+      </Modal>
+    </div>
+  )
+}
